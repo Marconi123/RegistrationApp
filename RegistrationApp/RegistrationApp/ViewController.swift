f10669d74ef2158d696d59804a0cc78e37a174a2
Vladislav <vladislavgalkin5@gmail.com>
2022-07-30 19:29:16 +0300

    New project created

---
+++ RegistrationApp/RegistrationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegistrationApp
+//
+//  Created by Владислав on 30.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
