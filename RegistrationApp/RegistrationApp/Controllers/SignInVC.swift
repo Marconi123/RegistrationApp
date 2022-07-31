a33de978d60191f34bec503f0dae0d607ec1a060
Vladislav <vladislavgalkin5@gmail.com>
2022-08-01 00:33:16 +0300

    SignUp(1st screen 90%) + SignIn

---
+++ RegistrationApp/RegistrationApp/Controllers/SignInVC.swift
@@ -0,0 +1,21 @@
+//
+//  SignInVC.swift
+//  RegistrationApp
+//
+//  Created by Владислав on 31.07.22.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    @IBOutlet weak var btnSignIn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        btnSignIn.frame.size.width = 125
+        btnSignIn.frame.size.height = 125
+        btnSignIn.layer.cornerRadius = btnSignIn.frame.width / 2
+        btnSignIn.layer.masksToBounds = true
+    }
+
+}
--- RegistrationApp/RegistrationApp/Controllers/SignUpVC.swift
+++ RegistrationApp/RegistrationApp/Controllers/SignUpVC.swift
@@ -9,13 +9,21 @@ import UIKit
 
 class SignUpVC: UIViewController {
 
-    @IBOutlet weak var btnSignIn: UIButton!
+    enum stateOfSecury {
+        case `false`
+        case `true`
+    }
+    
+    @IBOutlet weak var passwordInput: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        btnSignIn.frame.size.width = 125
-        btnSignIn.frame.size.height = 125
-        btnSignIn.layer.cornerRadius = btnSignIn.frame.width / 2
-        btnSignIn.layer.masksToBounds = true
+        passwordInput.isSecureTextEntry = true
     }
-
+    @IBAction func buttonPressed() {
+        if passwordInput.isSecureTextEntry == true {
+            passwordInput.isSecureTextEntry = false
+        } else {
+            passwordInput.isSecureTextEntry = true
+        }
+    }
 }
