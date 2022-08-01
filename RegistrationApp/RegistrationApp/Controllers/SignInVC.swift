358df856c37580a3f839b50ed873d4235605b177
Vladislav <vladislavgalkin5@gmail.com>
2022-08-01 15:44:23 +0300

    Update frames SignUp screen + SignIn

--- RegistrationApp/RegistrationApp/Controllers/SignInVC.swift
+++ RegistrationApp/RegistrationApp/Controllers/SignInVC.swift
@@ -10,8 +10,19 @@ import UIKit
 class SignInVC: UIViewController {
 
     @IBOutlet weak var btnSignIn: UIButton!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButtonOutl: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        emailField.attributedPlaceholder = NSAttributedString(string: "Email",
+                                                              attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password",
+                                                                                             attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
     }
-
+    @IBAction func createAnAccountBtn() {
+        performSegue(withIdentifier: "goToSignUp", sender: nil)
+    }
+    @IBAction func loginBtn() {
+    }
 }
